b8975bc95eaf89c33de60e68b8910ddf4e0d6178
bangprovn <bangprovn@gmail.com>
2016-05-06 12:48:50 +0700

    SettingsProvider: Enabled vibrate when ringing as default plus config for turning it off
    Change-Id: I98314f9cbe3a35b9d3cc3759a8959f69b4f50fd8

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2606,6 +2606,9 @@ class DatabaseHelper extends SQLiteOpenHelper {
             loadIntegerSetting(stmt, Settings.System.POINTER_SPEED,
                     R.integer.def_pointer_speed);
 
+            loadIntegerSetting(stmt, Settings.System.VIBRATE_WHEN_RINGING,
+                    R.integer.def_vib_when_ringing);
+
             /*
              * IMPORTANT: Do not add any more upgrade steps here as the global,
              * secure, and system settings are no longer stored in a database
