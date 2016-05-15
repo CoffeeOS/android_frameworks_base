d43f456ae9043e4c7f6462978a2903fb2b247481
bangprovn <bangprovn@gmail.com>
2016-05-15 13:12:24 +0700

    Merge branch 'cm-13.0' of https://github.com/Cyanogenmod/android_frameworks_base into tekos-1.0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -302,6 +302,8 @@ public class NotificationPanelView extends PanelView implements
     private LiveLockScreenController mLiveLockscreenController;
     private final GestureDetector mGestureDetector;
     private ViewLinker mViewLinker;
+    private final UnlockMethodCache mUnlockMethodCache;
+
 
     private enum SwipeLockedDirection {
         UNKNOWN,
@@ -432,6 +434,7 @@ public class NotificationPanelView extends PanelView implements
         Point point = new Point();
         display.getSize(point);
         mScreenHeight = point.y;
+        mUnlockMethodCache = UnlockMethodCache.getInstance(context);
     }
 
     public void setStatusBar(PhoneStatusBar bar) {
@@ -1174,7 +1177,8 @@ public class NotificationPanelView extends PanelView implements
             return true;
         }
 
-        if (isKeyguardInteractiveAndShowing() || mStatusBar.isKeyguardShowingMedia()) {
+        if (isKeyguardInteractiveAndShowing() || mStatusBar.isKeyguardShowingMedia() ||
+                (mUnlockMethodCache.isTrustManaged() && mAfforanceHelper.isOnLockIcon(event))) {
             return super.onInterceptTouchEvent(event);
         }
 
@@ -1369,7 +1373,8 @@ public class NotificationPanelView extends PanelView implements
             updateVerticalPanelPosition(event.getX());
         }
 
-        if (isKeyguardInteractiveAndShowing() || mStatusBar.isKeyguardShowingMedia()) {
+        if (isKeyguardInteractiveAndShowing() || mStatusBar.isKeyguardShowingMedia() ||
+                (mUnlockMethodCache.isTrustManaged() && mAfforanceHelper.isOnLockIcon(event))) {
             super.onTouchEvent(event);
             return true;
         }
@@ -1693,6 +1698,9 @@ public class NotificationPanelView extends PanelView implements
         if (statusBarState == StatusBarState.KEYGUARD ||
                 statusBarState == StatusBarState.SHADE_LOCKED) {
             updateDozingVisibilities(false /* animate */);
+        }
+        if (statusBarState == StatusBarState.KEYGUARD) {
+            mShowingExternalKeyguard = false;
         }
         resetVerticalPanelPosition();
         updateQsState();
@@ -2225,10 +2233,6 @@ public class NotificationPanelView extends PanelView implements
         updateNotificationTranslucency();
         updatePanelExpanded();
         mNotificationStackScroller.setShadeExpanded(!isFullyCollapsed());
-        if (mShowingExternalKeyguard && expandedHeight >= getMaxPanelHeight()) {
-            mStatusBar.unfocusKeyguardExternalView();
-            mShowingExternalKeyguard = false;
-        }
         if (DEBUG) {
             invalidate();
         }
