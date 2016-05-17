0250132998b8c77de2c5042de8c40d1761ef5b13
Roman Birg <roman@cyngn.com>
2016-05-17 13:12:39 -0700

    SystemUI: update qs tile hint and header view on locale change
    Ticket: CYNGNOS-2814
    
    Change-Id: I305ac5eca862ffa5c525ec6b39bfeddf03719227
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
@@ -1888,10 +1888,11 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
             for (TileRecord r : mRecords) {
                 r.tile.clearState();
             }
+            updateDetailText();
+            mQsPanelTop.updateResources();
             if (mListening) {
                 refreshAllTiles();
             }
-            updateDetailText();
         }
     }
 
